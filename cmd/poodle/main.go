64e89a5a1e037f191d5a44463bb2d8192871d62d
Kieran Colford <kieran@kcolford.com>
2017-06-02 18:03:01 -0400

    removed ssh server

--- cmd/poodle/main.go
+++ cmd/poodle/main.go
@@ -12,15 +12,12 @@ func run() (err error) {
 	kalidevice := flag.String("kali-dev", "", "block device to add encrypted kali linux encrypted partition for")
 	randsigid := flag.String("rand-sig", "", "identifier to place the random signature in")
 	pkg := flag.String("pkg", "main", "package to place generated code into")
-	sshaddr := flag.String("ssh", "", "`host:port` specification for a listening SSH server")
 	flag.Parse()
 	switch {
 	case *kalidevice != "":
 		err = poodle.KaliAddEncryptedPartition(*kalidevice)
 	case *randsigid != "":
 		err = poodle.GenerateRandomSignature(*randsigid, *pkg)
-	case *sshaddr != "":
-		err = poodle.SshServer(*sshaddr)
 	default:
 		flag.Usage()
 	}
